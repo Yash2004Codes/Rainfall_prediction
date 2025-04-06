d43d75d91f0e86f3d2a6f25da3b44b9a7d14eb82
yash sharma <yashjiakola@gmail.com>
2025-04-06 17:17:56 +0530

    updated py file

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 import pickle
-
+import os
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)
@@ -35,4 +35,8 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+
+ port = int(os.environ.get("PORT", 10000))
+ app.run(host='0.0.0.0', port=port)
+
